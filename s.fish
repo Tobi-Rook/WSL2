e90424eeb11fdd8810683f950d11641d7c44fba2
Tobias Ansey <tobias.ansey@outlook.com>
2019-06-23 15:14:57 +0200

    Update comments

--- s.fish
+++ s.fish
@@ -10,22 +10,22 @@ function s
 				cat ~/.config/fish/functions/help/s | less
 			case '*'
 
-				# Start statement 
-				if echo $argv[$s] | grep -i '^start' | grep -ivq '^[A-Z]:\\\\'
+				# Start statement (--> Windows Command Prompt)
+				if echo $argv[$s] | grep -i '^start' | grep -ivq '^[a-z]:\\\\'
 					echo "$argv[$s]" | cmd.exe > /dev/null 2> /dev/null
 
-				# Windows, WSL or Linux
+				# Windows file paths / WSL / Linux distributions
 				else 
 
-					#
+					# Insertion of placeholders for a few incompatible chars
 					set -l file_Path (readlink -f $argv[$s] |
 					tr Ä '{A' | tr Ö '{O' | tr Ü '{U' |
 					tr ä '{a' | tr ö '{o' | tr ü '{u' |
 					tr É '{E' | tr é '{e' | tr ß '{s' |
 					tr Ñ '{N' | tr ñ '{n')
 
-					# Windows
-					if echo $argv[$s] | grep -iq '^[A-Z]:\\\\'
+					# Passed Windows file path
+					if echo $argv[$s] | grep -iq '^[a-z]:\\\\'
 						set -l file_Path_Win (echo $file_Path | sed "s|$PWD||g" | cut -b 2-)
 
 						# .pdf file extension
@@ -33,30 +33,6 @@ function s
 							echo "\"$DISK:\Users\%USERNAME%\\$WSL_DIR\wslstart.bat\" pdf \"$file_Path_Win\" \"$DISK:\\$BROWSER\"" | cmd.exe > /dev/null 2> /dev/null &
 
 						# .exe / .bat / .lnk extension
-						else if echo $file_Path_Win | grep -iqE '.lnk$|.bat$|.exe$'
-							echo "\"$DISK:\Users\%USERNAME%\\$WSL_DIR\wslstart.bat\" exe \"$file_Path_Win\"" | cmd.exe > /dev/null 2> /dev/null &
-
-						# Other file extensions
-						else
-							echo "\"$DISK:\Users\%USERNAME%\\$WSL_DIR\wslstart.bat\" start \"$file_Path_Win\"" | cmd.exe > /dev/null 2> /dev/null &
-						end
-
-					# WSL
-					else if echo $file_Path | grep -iq /mnt/\[a-z\]/
-
-						#
-						set -l file_Path_Win (echo $file_Path | sed 's/\//\\\\/g' | cut -b 6- | sed 's/^\(.\{1\}\)/\1:/')
-
-						# .pdf file extension
-						if echo $file_Path_Win | grep -iq '.pdf$'
-							echo "\"$DISK:\Users\%USERNAME%\\$WSL_DIR\wslstart.bat\" pdf \"$file_Path_Win\" \"$DISK:\\$BROWSER\"" | cmd.exe > /dev/null 2> /dev/null &
-
-							# Temporary needed for execution with all currently developed canary / dev / beta builds of chromium based browsers
-							set -U pwd $PWD
-							fish -c 'wsl r "$pwd/debug.log" $BROWSER_E' > /dev/null &
-
-						# .exe / .bat / .lnk extension or whitespaces in the file's absolute path
-						# 
 						else if echo $file_Path_Win | grep -iqE '.exe$|.bat$|.lnk$'
 							echo "\"$DISK:\Users\%USERNAME%\\$WSL_DIR\wslstart.bat\" exe \"$file_Path_Win\"" | cmd.exe > /dev/null 2> /dev/null &
 
@@ -65,7 +41,30 @@ function s
 							echo "\"$DISK:\Users\%USERNAME%\\$WSL_DIR\wslstart.bat\" start \"$file_Path_Win\"" | cmd.exe > /dev/null 2> /dev/null &
 						end
 
-					# Linux distributions
+					# Execution within the WSL
+					else if echo $file_Path | grep -iq '/mnt/[a-z]/'
+
+						# Linux file path conversion
+						set -l file_Path_Win (echo $file_Path | sed 's/\//\\\\/g' | cut -b 6- | sed 's/^\(.\{1\}\)/\1:/')
+
+						# .pdf file extension
+						if echo $file_Path_Win | grep -iq '.pdf$'
+							echo "\"$DISK:\Users\%USERNAME%\\$WSL_DIR\wslstart.bat\" pdf \"$file_Path_Win\" \"$DISK:\\$BROWSER\"" | cmd.exe > /dev/null 2> /dev/null &
+
+							# Temporarily required if the function call results in opening a canary / dev / beta build of a Chromium-based browser
+							set -U pwd $PWD
+							fish -c 'wsl r "$pwd/debug.log" $BROWSER_E' > /dev/null &
+
+						# .exe / .bat / .lnk extension or whitespaces in the file's absolute path
+						else if echo $file_Path_Win | grep -iqE '.exe$|.bat$|.lnk$'
+							echo "\"$DISK:\Users\%USERNAME%\\$WSL_DIR\wslstart.bat\" exe \"$file_Path_Win\"" | cmd.exe > /dev/null 2> /dev/null &
+
+						# Other file extensions
+						else
+							echo "\"$DISK:\Users\%USERNAME%\\$WSL_DIR\wslstart.bat\" start \"$file_Path_Win\"" | cmd.exe > /dev/null 2> /dev/null &
+						end
+
+					# Execution within any Linux distribution
 					else
 						set -l WSL_DISTRO_DIR (echo WSL_(echo $WSL_DISTRO_NAME)_DIR)
 
@@ -76,7 +75,7 @@ function s
 							echo "\"$DISK:\Users\%USERNAME%\\$WSL_DIR\wslstart.bat\" pdf \"$DISK:\Users\%USERNAME%\\$file_Name\" \"$DISK:\\$BROWSER\"" | cmd.exe > /dev/null 2> /dev/null &
 							fish -c 'wsl r '/mnt/$DISK/users/$USER/$argv[$s]' $BROWSER_E' > /dev/null 2> /dev/null &
 
-						# 
+						# Check if the current distribution is registered as a valid distribution in the WSL (--> $WSL_X_DIR)
 						else if test -n $$WSL_DISTRO_DIR
 
 							# .exe / .bat / .lnk file extension
@@ -84,7 +83,7 @@ function s
 								echo "\"$DISK:\Users\%USERNAME%\\$WSL_DIR\wslstart.bat\" exe \"\\\\wsl\$\\$WSL_DISTRO_NAME\\$file_Path\"" | cmd.exe > /dev/null 2> /dev/null &
 
 							# No file extension
-							else if echo $argv[$s] | grep -ivq '\.'
+							else if echo $argv[$s] | grep -iqv '\.'
 								echo "\"$DISK:\Users\%USERNAME%\\$WSL_DIR\wslstart.bat\" no \"\\\\wsl\$\\$WSL_DISTRO_NAME\\$file_Path\"" | cmd.exe > /dev/null 2> /dev/null &
 
 							# Other file extensions
@@ -92,7 +91,7 @@ function s
 								echo "\"$DISK:\Users\%USERNAME%\\$WSL_DIR\wslstart.bat\" cmd \"\\\\wsl\$\\$WSL_DISTRO_NAME\\$file_Path\"" | cmd.exe > /dev/null 2> /dev/null & 
 							end
 
-						# Distribution is not compatible or its file system path needs to be stored in a new WSL_DIR variable
+						# Distribution is not compatible / Distribution needs to be registered (--> $WSL_X_DIR)
 						else
 							echo "Operation not supported."
 						end
