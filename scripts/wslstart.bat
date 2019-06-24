8b923558b302324807a5fd94243c6a00c3a892e2
Tobias Ansey <tobias.ansey@outlook.com>
2019-06-24 11:25:36 +0200

    Add wslstart.bat

---
+++ scripts/wslstart.bat
@@ -0,0 +1,31 @@
+IF "%1" == "start" (
+	SET file_Path=%~2
+	GOTO:Routines
+)
+
+SET file_Path=%2
+CALL :Replace file_Path
+GOTO:Execute
+
+:Replace
+FOR %%i IN ("{A=Ä" "{B=ä" "{C=Å" "{D=å" "{E=É" "{F=é" "{G=Ñ" "{H=ñ" "{I=Ö" "{J=ö" "{K=Ø" "{L=ø" "{M=ẞ" "{N=ß" "{O=Ü" "{P=ü") DO CALL SET "%1=%%%1:%%~i%%"
+GOTO:EOF
+
+:Execute
+IF "%1" == "pdf" (
+	%3 %file_Path%
+)
+IF "%1" == "no" (
+	notepad.exe %file_Path%
+)
+IF "%1" == "cmd" (
+	cmd.exe /c "%file_Path%"
+)
+IF "%1" == "exe" (
+	%file_Path%
+)
+IF "%1" == "start" (
+	%file_Path%
+)
+GOTO:EOF
+
