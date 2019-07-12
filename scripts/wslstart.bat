0a1c3ad5d1d2ba4223a3c2973f1335ff6a0b130d
Tobias Ansey <tobias.ansey@outlook.com>
2019-07-12 12:07:18 +0200

    Fix function calls within paths containing special characters

--- scripts/wslstart.bat
+++ scripts/wslstart.bat
@@ -1,5 +1,6 @@
+SETLOCAL EnableDelayedExpansion
 IF "%1" == "start" (
-	SET file_Path=%~2
+	SET file_Path="%~2"
 	GOTO:Routines
 )
 
@@ -21,14 +22,19 @@ IF "%1" == "pdf" (
 IF "%1" == "no" (
 	notepad.exe %file_Path%
 )
-IF "%1" == "cmd" (
-	cmd.exe /c "%file_Path%"
+IF "%1" == "no2" (
+	notepads %file_Path%
 )
 IF "%1" == "exe" (
 	%file_Path%
+	GOTO:EOF
 )
 IF "%1" == "start" (
 	%file_Path%
 )
+IF "%1" == "cmd" (
+	cmd.exe /c "%file_Path%"
+)
+
 GOTO:EOF
 
